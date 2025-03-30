e611c4a4560f26f6b6af9e5d5aba0d93d1b12583
Văn Thành <hackersvn1@gmail.com>
2025-03-30 21:03:19 +0700

    hehe

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,37 +1,23 @@
 #!/bin/sh
 
-# Cài đặt Ngrok từ link tải trực tiếp
-curl -sSL https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-stable-linux-amd64.tgz -o /tmp/ngrok.tgz \
-    && tar -xvzf /tmp/ngrok.tgz -C /usr/local/bin \
-    && chmod +x /usr/local/bin/ngrok \
-    && rm /tmp/ngrok.tgz
+# Cài đặt Ngrok
+curl -sSL https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-stable-linux-amd64.tgz -o /tmp/ngrok.tgz && \
+    tar -xvzf /tmp/ngrok.tgz -C /usr/local/bin && \
+    chmod +x /usr/local/bin/ngrok && rm /tmp/ngrok.tgz
 
-# Đăng nhập Ngrok
+# Đăng nhập Ngrok và chạy code-server
 ngrok config add-authtoken 2uOH2eOMZZ1t3uMKUvW0Q4EusoW_7q55DwZ9SxNR5NsnG2XB5
-
-# Chạy code-server với mật khẩu "ngcsl"
 PASSWORD="ngcsl" /usr/bin/code-server --bind-addr 0.0.0.0:8080 &
-
-# Đợi code-server khởi động
-sleep 5
-
-# Chạy Ngrok
 ngrok http 8080 &
-
-# Đợi Ngrok khởi động
-sleep 5
 
 # Lấy Public URL của Ngrok
 public_url=$(curl -s http://127.0.0.1:4040/api/tunnels | grep -o 'https://[^"]*')
 echo "Public URL của Ngrok: $public_url"
 
-# Chạy vòng lặp đếm thời gian (30 ngày)
+# Đếm thời gian (30 ngày)
 total_seconds=2592000
 while [ $total_seconds -gt 0 ]; do
-    hours=$(( total_seconds / 3600 ))
-    minutes=$(( (total_seconds % 3600) / 60 ))
-    seconds=$(( total_seconds % 60 ))
-    printf "Thời gian còn lại: %d giờ %d phút %d giây\n" "$hours" "$minutes" "$seconds"
+    printf "Thời gian còn lại: %d giờ %d phút %d giây\n" $(( total_seconds / 3600 )) $(( (total_seconds % 3600) / 60 )) $(( total_seconds % 60 ))
     sleep 1
     total_seconds=$(( total_seconds - 1 ))
 done
