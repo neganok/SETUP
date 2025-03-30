47e35e23aa8c166035bef8f0cd085889b64ed983
Văn Thành <hackersvn1@gmail.com>
2025-03-30 21:59:28 +0700

    cac

--- start.sh
+++ start.sh
@@ -6,12 +6,12 @@ curl -sSL https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-stable-linux-amd64.tgz -o /
     tar -xvzf /tmp/ngrok.tgz -C /usr/local/bin && \
     chmod +x /usr/local/bin/ngrok && rm /tmp/ngrok.tgz
 
-# Cài đặt code-server phiên bản 4.98.2
+# Cài đặt code-server
 echo "Cài đặt code-server..."
-curl -fsSL https://github.com/coder/code-server/releases/download/v4.98.2/code-server-4.98.2-linux-amd64.tar.gz -o /tmp/code-server.tar.gz && \
-    tar -xvzf /tmp/code-server.tar.gz -C /usr/local/bin && \
-    chmod +x /usr/local/bin/code-server && \
-    rm /tmp/code-server.tar.gz
+curl -fsSL https://github.com/coder/code-server/releases/download/v4.98.2/code-server-4.98.2-linux-amd64.tar.gz -o /tmp/code-server.tar.gz
+tar -xvzf /tmp/code-server.tar.gz -C /usr/local/bin
+chmod +x /usr/local/bin/code-server
+rm /tmp/code-server.tar.gz
 
 # Đảm bảo code-server có thể chạy
 export PATH=$PATH:/usr/local/bin
