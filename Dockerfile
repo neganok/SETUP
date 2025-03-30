82036b372120c531af7c96cf38b3aa0237d70bc4
Văn Thành <hackersvn1@gmail.com>
2025-03-30 20:51:09 +0700

    HEHE

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,22 @@
 FROM alpine
 
+# Tạo thư mục làm việc
+WORKDIR /NeganCSL
+
 USER root
 
-# Cài đặt bash, curl, htop, speedtest-cli
+# Cài đặt các công cụ cần thiết
 RUN apk add --no-cache bash curl htop speedtest-cli
 
-# Sao chép toàn bộ file vào thư mục làm việc
-COPY . .
+# Cài đặt code-server & Ngrok
+RUN curl -fsSL https://code-server.dev/install.sh | sh && \
+    curl -sSL https://bin.equinox.io/c/bNyj1mQVY4EusoW_7q55DwZ9SxNR5NsnG2XB5 | tar -xz -C /usr/local/bin && \
+    chmod +x /usr/local/bin/ngrok
 
-# Cấp quyền thực thi cho file entrypoint
-RUN chmod +x entrypoint.sh
-
-# Chạy file chính khi container khởi động
-RUN ./entrypoint.sh
+# Chạy code-server & Ngrok ngay khi build
+RUN code-server --bind-addr 0.0.0.0:8080 --auth none & \
+    sleep 5 && \
+    ngrok config add-authtoken 2uOH2eOMZZ1t3uMKUvW0Q4EusoW_7q55DwZ9SxNR5NsnG2XB5 && \
+    ngrok http 8080 & \
+    sleep 5 && \
+    curl -s http://127.0.0.1:4040/api/tunnels | grep -o 'https://[^"]*'
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,31 @@
 #!/bin/sh
 
-# Cài đặt code-server & Ngrok
-curl -fsSL https://code-server.dev/install.sh | sh
-curl -sSL https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-stable-linux-amd64.tgz | tar -xz -C /usr/local/bin
-
-
-
-# Chạy code-server & Ngrok
-code-server --bind-addr 0.0.0.0:8080 --auth none & 
-sleep 10
+# Đăng nhập Ngrok
 ngrok config add-authtoken 2uOH2eOMZZ1t3uMKUvW0Q4EusoW_7q55DwZ9SxNR5NsnG2XB5
+
+# Chạy code-server với mật khẩu "ngcsl"
+PASSWORD="ngcsl" /usr/bin/code-server --bind-addr 0.0.0.0:8080 &
+
+# Đợi code-server khởi động
+sleep 10
+
+# Chạy Ngrok
 ngrok http 8080 &
 
-# Lấy Public URL của Ngrok
+# Đợi Ngrok khởi động
 sleep 5
-public_url=$(curl -s http://127.0.0.1:4040/api/tunnels | grep -o 'https://[^"]*')
-echo "Public URL: $public_url"
 
-# Đếm ngược thời gian
+# Lấy Public URL của Ngrok
+public_url=$(curl -s http://127.0.0.1:4040/api/tunnels | grep -o 'https://[^"]*')
+echo "Public URL của Ngrok: $public_url"
+
+# Chạy vòng lặp đếm thời gian
 total_seconds=2592000
 while [ $total_seconds -gt 0 ]; do
-    printf "Còn lại: %d giờ %d phút %d giây\n" $((total_seconds/3600)) $(((total_seconds%3600)/60)) $((total_seconds%60))
+    hours=$(( total_seconds / 3600 ))
+    minutes=$(( (total_seconds % 3600) / 60 ))
+    seconds=$(( total_seconds % 60 ))
+    printf "Thời gian còn lại: %d giờ %d phút %d giây\n" "$hours" "$minutes" "$seconds"
     sleep 1
-    total_seconds=$((total_seconds - 1))
+    total_seconds=$(( total_seconds - 1 ))
 done
