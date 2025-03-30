569077756612a531877e5467c68a99653d16b903
Văn Thành <hackersvn1@gmail.com>
2025-03-30 21:08:57 +0700

    cac

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
-# Cài đặt Ngrok
-curl -sSL https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-stable-linux-amd64.tgz -o /tmp/ngrok.tgz && \
-    tar -xvzf /tmp/ngrok.tgz -C /usr/local/bin && \
-    chmod +x /usr/local/bin/ngrok && rm /tmp/ngrok.tgz
+# Cài đặt Node.js LTS
+curl -fsSL https://raw.githubusercontent.com/tj/n/master/bin/n | bash -s lts
 
-# Đăng nhập Ngrok và chạy code-server
-ngrok config add-authtoken 2uOH2eOMZZ1t3uMKUvW0Q4EusoW_7q55DwZ9SxNR5NsnG2XB5
-PASSWORD="ngcsl" /usr/bin/code-server --bind-addr 0.0.0.0:8080 &
-ngrok http 8080 &
+# Cài đặt code-server
+curl -fsSL https://code-server.dev/install.sh | sh && \
+code-server --bind-addr 0.0.0.0:8080 --auth none & \
 
-# Lấy Public URL của Ngrok
-public_url=$(curl -s http://127.0.0.1:4040/api/tunnels | grep -o 'https://[^"]*')
-echo "Public URL của Ngrok: $public_url"
+# Cài đặt ngrok và đăng nhập
+npm install -g ngrok && \
+ngrok authtoken 2uOH2eOMZZ1t3uMKUvW0Q4EusoW_7q55DwZ9SxNR5NsnG2XB5 && \
+ngrok http 8080 & \
 
-# Đếm thời gian (30 ngày)
-total_seconds=2592000
+# Đợi ngrok khởi động và lấy public URL
+sleep 5 && \
+public_url=$(curl -s http://127.0.0.1:4040/api/tunnels | grep -o 'https://[^"]*') && \
+echo "Public URL của ngrok: $public_url" && \
+
+# Đếm ngược 30 ngày
+total_seconds=2592000  # 30 ngày = 2.592.000 giây
 while [ $total_seconds -gt 0 ]; do
-    printf "Thời gian còn lại: %d giờ %d phút %d giây\n" $(( total_seconds / 3600 )) $(( (total_seconds % 3600) / 60 )) $(( total_seconds % 60 ))
+    hours=$((total_seconds / 3600))
+    minutes=$(( (total_seconds % 3600) / 60 ))
+    seconds=$((total_seconds % 60))
+    printf "Thời gian còn lại: %02d giờ %02d phút %02d giây\n" $hours $minutes $seconds
     sleep 1
-    total_seconds=$(( total_seconds - 1 ))
+    total_seconds=$((total_seconds - 1))
 done
