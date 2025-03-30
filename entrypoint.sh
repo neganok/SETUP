7177ebf3d564e4978d01c5d0744ec4edc2cd8513
Văn Thành <hackersvn1@gmail.com>
2025-03-30 19:36:18 +0700

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Đăng nhập Ngrok
+ngrok authtoken 2uOH2eOMZZ1t3uMKUvW0Q4EusoW_7q55DwZ9SxNR5NsnG2XB5
+
+# Chạy code-server
+/usr/local/bin/code-server --bind-addr 0.0.0.0:8080 &
+
+# Đợi 10 giây để code-server khởi động
+sleep 10
+
+# Chạy Ngrok
+ngrok http 8080 &
+
+# Đợi Ngrok khởi động
+sleep 10
+
+# Lấy Public URL của Ngrok
+public_url=$(curl -s http://127.0.0.1:4040/api/tunnels | grep -o 'https://[^"]*')
+echo "Public URL của ngrok: $public_url"
+
+# Chạy vòng lặp đếm thời gian
+total_seconds=2592000
+while [ $total_seconds -gt 0 ]; do
+    hours=$(( total_seconds / 3600 ))
+    minutes=$(( (total_seconds % 3600) / 60 ))
+    seconds=$(( total_seconds % 60 ))
+    printf "Thời gian còn lại: %d giờ %d phút %d giây\n" "$hours" "$minutes" "$seconds"
+    sleep 1
+    total_seconds=$(( total_seconds - 1 ))
+done
